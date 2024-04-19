d37170e4feb62a86f173979712fee28cd8e8a24a
QuentinDV <148322258+QuentinDV@users.noreply.github.com>
2024-04-20 00:24:10 +0200

    pages added

--- main.go
+++ main.go
@@ -10,15 +10,36 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./html/home.html")
 }
 
+func Categories(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./html/categories.html")
+}
+
+func LogIn(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./html/login.html")
+}
+
+func SignUp(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./html/signup.html")
+}
+
+func Settings(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./html/settings.html")
+}
+
 func main() {
 	// Pages
 	http.HandleFunc("/home", Home)
+	http.HandleFunc("/categories", Home)
+	http.HandleFunc("/login", Home)
+	http.HandleFunc("/signup", Home)
+	http.HandleFunc("/settings", Home)
 
 	// Elements
-	http.Handle("/assets/static/", http.StripPrefix("/assets/static/", http.FileServer(http.Dir("./assets/static"))))
+	http.Handle("/css", http.StripPrefix("/css", http.FileServer(http.Dir("./css"))))
 	http.Handle("/assets/img/", http.StripPrefix("/assets/img/", http.FileServer(http.Dir("./assets/img"))))
 
 	// Liens
 	fmt.Println("\nPlay : http://localhost:8080/home")
+	fmt.Println("\nPlay : http://localhost:8080/settings")
 	http.ListenAndServe(":8080", nil)
 }
