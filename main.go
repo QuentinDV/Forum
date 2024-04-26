0a02277bae532acd7f36681e899bd2a5586fbe55
QuentinDV <dassivignonquentin@gmail.com>
2024-04-26 12:17:11 +0200

    html fix

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
-import "forum/assets/go/database"
+import (
+	"fmt"
+	"forum/assets/go/web"
+	"net/http"
+)
 
 func main() {
-	database.CreateAccount("quentin.dassivignon@ynov.com", "quentin123", "blazefast")
+	// database.CreateAccount("quentin.dassivignon@ynov.com", "quentin123", "blazefast")
 
-	// // Pages
-	// http.HandleFunc("/home", web.Home)
-	// http.HandleFunc("/categories", web.Categories)
-	// http.HandleFunc("/login", web.LogIn)
-	// http.HandleFunc("/signup", web.SignUp)
-	// http.HandleFunc("/settings", web.Settings)
+	// Pages
+	http.HandleFunc("/home", web.Home)
+	http.HandleFunc("/categories", web.Categories)
+	http.HandleFunc("/login", web.LogIn)
+	http.HandleFunc("/signup", web.SignUp)
+	http.HandleFunc("/settings", web.Settings)
 
-	// // Elements
-	// http.Handle("/css", http.StripPrefix("/css", http.FileServer(http.Dir("./css"))))
-	// http.Handle("/assets/img/", http.StripPrefix("/assets/img/", http.FileServer(http.Dir("./assets/img"))))
+	// Elements
+	http.Handle("/assets/css/", http.StripPrefix("/assets/css/", http.FileServer(http.Dir("./assets/css"))))
+	http.Handle("/assets/js/", http.StripPrefix("/assets/js/", http.FileServer(http.Dir("./assets/js"))))
+	http.Handle("/assets/img/", http.StripPrefix("/assets/img/", http.FileServer(http.Dir("./assets/img"))))
 
-	// // Liens
-	// fmt.Println("\nPlay : http://localhost:8080/home")
-	// fmt.Println("\nPlay : http://localhost:8080/login")
-	// http.ListenAndServe(":8080", nil)
+	// Liens
+	fmt.Println("\nPlay : http://localhost:8080/home")
+	fmt.Println("\nPlay : http://localhost:8080/login")
+	http.ListenAndServe(":8080", nil)
 }
