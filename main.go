810cce865701c51cd7e86e7e4301cde8c51929eb
dowandji <owandji.dieng@ynov.com>
2024-04-26 12:21:50 +0200

    ajouts css dans le go

--- main.go
+++ main.go
@@ -15,10 +15,13 @@ func main() {
 	http.HandleFunc("/settings", web.Settings)
 
 	// Elements
-	http.Handle("/css", http.StripPrefix("/css", http.FileServer(http.Dir("./css"))))
+	http.Handle("/assets/js/", http.StripPrefix("/assets/js/", http.FileServer(http.Dir("assets/js"))))
+	http.Handle("/assets/css/", http.StripPrefix("/assets/css/", http.FileServer(http.Dir("assets/css"))))
 	http.Handle("/assets/img/", http.StripPrefix("/assets/img/", http.FileServer(http.Dir("./assets/img"))))
 
 	// Liens
+	fmt.Println("\nPlay : http://localhost:8080/categories")
+	fmt.Println("\nPlay : http://localhost:8080/login")
 	fmt.Println("\nPlay : http://localhost:8080/home")
 	fmt.Println("\nPlay : http://localhost:8080/settings")
 	http.ListenAndServe(":8080", nil)
