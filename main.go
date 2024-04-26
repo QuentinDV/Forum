c9fc277921852b66ecd1709a92e94847179b8481
QuentinDV <dassivignonquentin@gmail.com>
2024-04-26 15:51:29 +0200

    account creation working

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 	"net/http"
 )
 
-var ConnectedAccount = database.Account{Id: "0", Username: "Guest", ImageUrl: ""}
-
 func main() {
+	database.CreateAccount("quentin.dassivignon@ynov.com", "quentin123", "quentindv", true)
+	database.CreateAccount("owandji.dieng@ynov.com", "owandji123", "owandji", true)
+
 	// Pages
 	http.HandleFunc("/home", web.Home)
 	http.HandleFunc("/categories", web.Categories)
@@ -17,18 +18,15 @@ func main() {
 	http.HandleFunc("/signup", web.SignUp)
 	http.HandleFunc("/settings", web.Settings)
 
-	//Forms
-	http.HandleFunc("/signup", web.SignUpForm)
-
 	// Elements
 	http.Handle("/assets/css/", http.StripPrefix("/assets/css/", http.FileServer(http.Dir("./assets/css"))))
 	http.Handle("/assets/js/", http.StripPrefix("/assets/js/", http.FileServer(http.Dir("./assets/js"))))
 	http.Handle("/assets/img/", http.StripPrefix("/assets/img/", http.FileServer(http.Dir("./assets/img"))))
 
 	// Liens
-	fmt.Println("\nPlay : http://localhost:8080/settings")
 	fmt.Println("\nPlay : http://localhost:8080/categories")
 	fmt.Println("\nPlay : http://localhost:8080/login")
 	fmt.Println("\nPlay : http://localhost:8080/home")
+	fmt.Println("\nPlay : http://localhost:8080/login")
 	http.ListenAndServe(":8080", nil)
 }
